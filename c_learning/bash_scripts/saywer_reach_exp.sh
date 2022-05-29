ddef13631434f45c0725c6f7f231e499e61c7fa5
Chongyi Zheng <chongyiz@chongyis-mbp.wifi.local.cmu.edu>
2022-05-29 18:47:51 -0400

    run exp for 600k

--- c_learning/bash_scripts/saywer_reach_exp.sh
+++ c_learning/bash_scripts/saywer_reach_exp.sh
@@ -14,10 +14,11 @@ for seed in "${seeds[@]}"; do
   python $PROJECT_DIR/train_eval.py \
     --gin_bindings="train_eval.env_name='sawyer_reach'" \
     --gin_bindings="train_eval.random_seed=${seed}" \
+    --gin_bindings="train_eval.num_iterations=600000" \
     --gin_bindings="obs_to_goal.start_index=0" \
     --gin_bindings="obs_to_goal.end_index=3" \
     --gin_bindings="goal_fn.relabel_next_prob=0.5" \
     --gin_bindings="goal_fn.relabel_future_prob=0.0" \
-    --root_dir ~/offline_c_learning/c_learning_logs/sawyer_reach/$seed \
+    --root_dir ~/offline_c_learning/c_learning_logs/sawyer_reach_600k/$seed \
   > ~/offline_c_learning/terminal_logs/sawyer_reach-"$seed".log 2>&1 &
 done
