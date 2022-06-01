c76c5d9f26cd69c785141e42d4ff0023cf876253
Chongyi Zheng <chongyiz@Chongyis-MacBook-Pro.local>
2022-06-01 02:00:21 -0400

    update sawyer_drawer and sawyer_window exp script

--- c_learning/bash_scripts/saywer_drawer_exp.sh
+++ c_learning/bash_scripts/saywer_drawer_exp.sh
@@ -6,19 +6,19 @@ PROJECT_DIR=$(realpath "$SCRIPT_DIR/..")
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/.mujoco/mujoco200/bin
 export PYTHONPATH=$PROJECT_DIR
 
-declare -a seeds=(1)
+declare -a seeds=(0 1 2)
 
 for seed in "${seeds[@]}"; do
-  export CUDA_VISIBLE_DEVICES=7
+  export CUDA_VISIBLE_DEVICES=6
   nohup \
   python $PROJECT_DIR/train_eval.py \
-    --gin_bindings="train_eval.env_name='sawyer_reach'" \
+    --gin_bindings="train_eval.env_name='sawyer_drawer'" \
     --gin_bindings="train_eval.random_seed=${seed}" \
-    --gin_bindings="train_eval.num_iterations=600000" \
-    --gin_bindings="obs_to_goal.start_index=0" \
-    --gin_bindings="obs_to_goal.end_index=3" \
-    --gin_bindings="goal_fn.relabel_next_prob=0.5" \
-    --gin_bindings="goal_fn.relabel_future_prob=0.0" \
-    --root_dir ~/offline_c_learning/c_learning_logs/sawyer_reach_600k/$seed \
-  > ~/offline_c_learning/terminal_logs/sawyer_reach_600k-"$seed".log 2>&1 &
+    --gin_bindings="train_eval.num_iterations=3000000" \
+    --gin_bindings="train_eval.log_subset=(3, None)" \
+    --gin_bindings="goal_fn.relabel_next_prob=0.3" \
+    --gin_bindings="goal_fn.relabel_future_prob=0.2" \
+    --gin_bindings="SawyerDrawer.reset.arm_goal_type='goal'" \
+    --root_dir ~/offline_c_learning/c_learning_logs/sawyer_drawer/$seed \
+  > ~/offline_c_learning/terminal_logs/sawyer_drawer-"$seed".log 2>&1 &
 done
