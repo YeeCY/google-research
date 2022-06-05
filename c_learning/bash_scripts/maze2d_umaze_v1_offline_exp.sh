d4068c635c750c5a40165658e01089903a9fb685
Chongyi Zheng <chongyiz@chongyis-mbp.wifi.local.cmu.edu>
2022-06-05 15:31:01 -0400

    add mazed2d_umaze_v1 offline exp

--- c_learning/bash_scripts/maze2d_umaze_v1_offline_exp.sh
+++ c_learning/bash_scripts/maze2d_umaze_v1_offline_exp.sh
@@ -9,7 +9,7 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/.mujoco/mujoco200/bin
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/nvidia-000
 export PYTHONPATH=$PROJECT_DIR
 
-declare -a seeds=(1)
+declare -a seeds=(0 1 2)
 
 for seed in "${seeds[@]}"; do
   export CUDA_VISIBLE_DEVICES=$GPUS
