c772865448af1d0d66c99666618cffc4e5709d1d
Chongyi Zheng <chongyiz@Chongyis-MacBook-Pro.local>
2022-06-09 22:10:11 -0400

    update maze2d_large_v1_relabel_with_next_and_future_exp.sh

--- c_learning/bash_scripts/maze2d_large_v1_relabel_with_next_and_future_exp.sh
+++ c_learning/bash_scripts/maze2d_large_v1_relabel_with_next_and_future_exp.sh
@@ -20,8 +20,8 @@ for seed in "${seeds[@]}"; do
     --gin_bindings="train_eval.num_iterations=1000000" \
     --gin_bindings="obs_to_goal.start_index=0" \
     --gin_bindings="obs_to_goal.end_index=2" \
-    --gin_bindings="goal_fn.relabel_next_prob=0.5" \
-    --gin_bindings="goal_fn.relabel_future_prob=0.0" \
+    --gin_bindings="goal_fn.relabel_next_prob=0.3" \
+    --gin_bindings="goal_fn.relabel_future_prob=0.2" \
     --root_dir ~/offline_c_learning/c_learning_logs/"${EXP_LABEL}"_relabel_with_next_and_future/maze2d_large_v1/$seed \
   > ~/offline_c_learning/c_learning_logs/"${EXP_LABEL}"_relabel_with_next_and_future/maze2d_large_v1/$seed/stream.log 2>&1 &
 done
