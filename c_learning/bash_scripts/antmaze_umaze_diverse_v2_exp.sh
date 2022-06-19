70db63c34e914e6852d8e86e2319504d2ff9517f
Chongyi Zheng <chongyiz@chongyis-mbp.wifi.local.cmu.edu>
2022-06-19 17:50:48 -0400

    update scripts

--- c_learning/bash_scripts/antmaze_umaze_diverse_v2_exp.sh
+++ c_learning/bash_scripts/antmaze_umaze_diverse_v2_exp.sh
@@ -25,5 +25,6 @@ for seed in "${seeds[@]}"; do
     --gin_bindings="goal_fn.relabel_next_prob=0.3" \
     --gin_bindings="goal_fn.relabel_future_prob=0.2" \
     --root_dir ~/offline_c_learning/c_learning_logs/"${EXP_LABEL}"/antmaze_umaze_diverse_v2/$seed \
-  > ~/offline_c_learning/c_learning_logs/"${EXP_LABEL}"/antmaze_umaze_diverse_v2/$seed/stream.log 2>&1 &
+  > ~/offline_c_learning/c_learning_logs/"${EXP_LABEL}"/antmaze_umaze_diverse_v2/$seed/stream.log 2>&1 & \
+  sleep 2
 done
