2c976ceb88173ea59450a41ad42c0ceeae49d4ab
Chongyi Zheng <chongyiz@chongyis-mbp.wifi.local.cmu.edu>
2022-06-19 14:07:09 -0400

    maze2d-open-v0 offline policy ratio with smaller policy_log_ratio_clip_max

--- c_learning/bash_scripts/maze2d_open_v0_offline_policy_ratio_setting_c_exp.sh
+++ c_learning/bash_scripts/maze2d_open_v0_offline_policy_ratio_setting_c_exp.sh
@@ -28,6 +28,7 @@ for seed in "${seeds[@]}"; do
     --gin_bindings="offline_goal_fn.setting='c'" \
     --gin_bindings="offline_c_learning_agent.actor_loss.ce_loss=True" \
     --gin_bindings="offline_c_learning_agent.critic_loss.policy_ratio=True" \
+    --gin_bindings="offline_c_learning_agent.critic_loss.policy_log_ratio_clip_max=2" \
     --root_dir ~/offline_c_learning/c_learning_offline_logs/"${EXP_LABEL}"/maze2d_open_v0/$seed \
   > ~/offline_c_learning/c_learning_offline_logs/"${EXP_LABEL}"/maze2d_open_v0/$seed/stream.log 2>&1 & \
   sleep 2
