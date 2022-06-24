8320c4d138f3ae2695e34d8e3ffc2554a978a11c
Chongyi Zheng <chongyiz@Chongyis-MacBook-Pro.local>
2022-06-24 01:38:50 -0400

    update metaworld exp scripts

--- c_learning/bash_scripts/metaworld_button_press_exp.sh
+++ c_learning/bash_scripts/metaworld_button_press_exp.sh
@@ -21,8 +21,8 @@ for seed in "${seeds[@]}"; do
     --gin_bindings="train_eval.max_future_steps=200" \
     --gin_bindings="train_eval.random_seed=${seed}" \
     --gin_bindings="train_eval.num_iterations=1000000" \
-    --gin_bindings="obs_to_goal.start_index=0" \
-    --gin_bindings="obs_to_goal.end_index=3" \
+    --gin_bindings="obs_to_goal.start_index=22" \
+    --gin_bindings="obs_to_goal.end_index=25" \
     --gin_bindings="goal_fn.relabel_next_prob=0.5" \
     --gin_bindings="goal_fn.relabel_future_prob=0.0" \
     --root_dir ~/offline_c_learning/c_learning_logs/"${EXP_LABEL}"/metaworld_button_press/$seed \
