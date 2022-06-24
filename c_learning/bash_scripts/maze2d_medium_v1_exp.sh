fd566236bdbb80e6e519790b8e4e1cb6db706c26
Chongyi Zheng <chongyiz@chongyis-mbp.wifi.local.cmu.edu>
2022-06-24 18:35:43 -0400

    update maze2d exp scripts

--- c_learning/bash_scripts/maze2d_medium_v1_exp.sh
+++ c_learning/bash_scripts/maze2d_medium_v1_exp.sh
@@ -18,8 +18,8 @@ for seed in "${seeds[@]}"; do
     --gin_bindings="train_eval.env_name='maze2d-medium-v1'" \
     --gin_bindings="train_eval.random_seed=${seed}" \
     --gin_bindings="train_eval.num_iterations=1000000" \
-    --gin_bindings="obs_to_goal.start_index=0" \
-    --gin_bindings="obs_to_goal.end_index=2" \
+    --gin_bindings="obs_to_goal.start_index=(0,)" \
+    --gin_bindings="obs_to_goal.end_index=(2,)" \
     --gin_bindings="goal_fn.relabel_next_prob=0.5" \
     --gin_bindings="goal_fn.relabel_future_prob=0.0" \
     --root_dir ~/offline_c_learning/c_learning_logs/"${EXP_LABEL}"/maze2d_medium_v1/$seed \
