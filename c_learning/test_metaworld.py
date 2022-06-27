9ec5802e67fc109c8afe0edb8e13006f7aadaa7d
Chongyi Zheng <chongyiz@Chongyis-MacBook-Pro.local>
2022-06-26 21:03:53 -0400

    update test_metaworld.py

--- c_learning/test_metaworld.py
+++ c_learning/test_metaworld.py
@@ -1,14 +1,79 @@
 from absl import app
 from absl import flags
 
+import gym
 import numpy as np
 
+from metaworld.envs.mujoco.env_dict import ALL_V2_ENVIRONMENTS
 from metaworld.envs import (ALL_V2_ENVIRONMENTS_GOAL_OBSERVABLE,
                             ALL_V2_ENVIRONMENTS_GOAL_HIDDEN)
+
+from PIL import Image
+
 
 flags.DEFINE_string('env_name', 'reach-v2', 'Name of metaworld task.')
 
 FLAGS = flags.FLAGS
+
+
+class SawyerDrawer(ALL_V2_ENVIRONMENTS['drawer-close-v2']):
+    """Wrapper for the SawyerDrawer environment."""
+
+    def __init__(self):
+        super(SawyerDrawer, self).__init__()
+        self._random_reset_space.low[0] = 0
+        self._random_reset_space.high[0] = 0
+        self._partially_observable = False
+        self._freeze_rand_vec = False
+        self._set_task_called = True
+        self._target_pos = np.zeros(0)  # We will overwrite this later.
+        self.reset()
+        self._freeze_rand_vec = False  # Set False to randomize the goal position.
+
+    def _get_pos_objects(self):
+        return self.get_body_com('drawer_link') + np.array([.0, -.16, 0.0])
+
+    def reset_model(self):
+        super(SawyerDrawer, self).reset_model()
+        self._set_obj_xyz(np.random.uniform(-0.15, 0.0))
+        img = self.render(offscreen=True)
+        img = Image.fromarray(img)
+        img.save("/data/chongyiz/offline_c_learning/debug")
+
+        self._target_pos = self._get_pos_objects().copy()
+
+        self._set_obj_xyz(np.random.uniform(-0.15, 0.0))
+        return self._get_obs()
+
+    @property
+    def observation_space(self):
+        return gym.spaces.Box(
+            low=np.full(8, -np.inf),
+            high=np.full(8, np.inf),
+            dtype=np.float32)
+
+    def _get_obs(self):
+        finger_right, finger_left = (self._get_site_pos('rightEndEffector'),
+                                     self._get_site_pos('leftEndEffector'))
+        tcp_center = (finger_right + finger_left) / 2.0
+        obj = self._get_pos_objects()
+        # Arm position is same as drawer position. We only provide the drawer
+        # Y coordinate.
+        return np.concatenate([tcp_center, [obj[1]],
+                               self._target_pos, [self._target_pos[1]]])
+
+    def step(self, action):
+        obs = super(SawyerDrawer, self).step(action)
+        return obs, 0.0, False, {}
+
+
+def main_sawyer(_):
+    env = SawyerDrawer()
+    obs = env.reset()
+    action = env.action_space.sample()
+    obs, reward, done, info = env.step(action)
+
+    print()
 
 
 def main(_):
@@ -48,10 +113,12 @@ def main(_):
     next_obs1, _, _, _ = env1.step(a1)  # Step the environoment with the sampled random action
     next_obs3, _, _, _ = env3.step(a3)
 
-    assert not (next_obs1[-3:] == next_obs3[-3:]).all()  # 2 envs initialized with different seeds will have different goals
+    assert not (next_obs1[-3:] == next_obs3[
+                                  -3:]).all()  # 2 envs initialized with different seeds will have different goals
     assert not (next_obs1[-3:] == np.zeros(
         3)).all()  # The env's are goal observable, meaning the goal is not zero'd out
 
 
 if __name__ == "__main__":
-    app.run(main)
+    # app.run(main)
+    app.run(main_sawyer)
