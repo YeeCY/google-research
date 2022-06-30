43ab0c297ad615edab95d2976e1808fdfc17f2ea
Chongyi Zheng <chongyiz@chongyis-mbp.wifi.local.cmu.edu>
2022-06-30 18:53:10 -0400

    fix aw_loss bugs

--- c_learning/bash_scripts/maze2d_open_v0_offline_aw_loss_setting_c_exp.sh
+++ c_learning/bash_scripts/maze2d_open_v0_offline_aw_loss_setting_c_exp.sh
@@ -21,15 +21,12 @@ for seed in "${seeds[@]}"; do
     --gin_bindings="train_eval_offline.random_seed=${seed}" \
     --gin_bindings="train_eval_offline.num_iterations=1000000" \
     --gin_bindings="train_eval_offline.max_future_steps=50" \
-    --gin_bindings="obs_to_goal.start_index=0" \
-    --gin_bindings="obs_to_goal.end_index=2" \
+    --gin_bindings="obs_to_goal.start_index=(0,)" \
+    --gin_bindings="obs_to_goal.end_index=(2,)" \
     --gin_bindings="offline_goal_fn.relabel_next_prob=0.5" \
     --gin_bindings="offline_goal_fn.relabel_next_future_prob=0.0" \
     --gin_bindings="offline_goal_fn.setting='c'" \
-    --gin_bindings="offline_c_learning_agent.actor_loss.ce_loss=False" \
-    --gin_bindings="offline_c_learning_agent.actor_loss.bc_loss=False" \
     --gin_bindings="offline_c_learning_agent.actor_loss.aw_loss=True" \
-    --gin_bindings="offline_c_learning_agent.critic_loss.policy_ratio=False" \
     --root_dir ~/offline_c_learning/c_learning_offline_logs/"${EXP_LABEL}"/maze2d_open_v0/$seed \
   > ~/offline_c_learning/c_learning_offline_logs/"${EXP_LABEL}"/maze2d_open_v0/$seed/stream.log 2>&1 & \
   sleep 2
