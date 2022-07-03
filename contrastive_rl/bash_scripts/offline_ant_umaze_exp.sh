469d98607f8a5a3e00e3e454815703f2338d3822
Chongyi Zheng <chongyiz@Chongyis-MacBook-Pro.local>
2022-07-03 11:54:57 -0400

    fix bug

--- contrastive_rl/bash_scripts/offline_ant_umaze_exp.sh
+++ contrastive_rl/bash_scripts/offline_ant_umaze_exp.sh
@@ -19,10 +19,11 @@ for seed in "${seeds[@]}"; do
   rm $CONDA_PREFIX/lib/python*/site-packages/mujoco_py/generated/mujocopy-buildlock
   mkdir -p ~/offline_c_learning/contrastive_rl_logs/"${EXP_LABEL}"/offline_ant_umaze/$seed
   nohup \
-  python $PROJECT_DIR/train_eval.py \
+  python $PROJECT_DIR/lp_contrastive.py \
     --env_name=offline_ant_umaze \
     --alg=c_learning \
     --seed="$seed" \
+    --lp_launch_type=local_mt \
     --log_dir=~/offline_c_learning/contrastive_rl_logs/"${EXP_LABEL}"/offline_ant_umaze/$seed \
   > ~/offline_c_learning/contrastive_rl_logs/"${EXP_LABEL}"/offline_ant_umaze/$seed/stream.log 2>&1 &
 done
