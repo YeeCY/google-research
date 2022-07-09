ea0a2653341ce6b7210274695382dbd572668354
Chongyi Zheng <chongyiz@chongyis-mbp.wifi.local.cmu.edu>
2022-07-09 18:05:06 -0400

    add experiment script

--- contrastive_rl/bash_scripts/offline_ant_umaze_exp.sh
+++ contrastive_rl/bash_scripts/offline_ant_umaze_exp.sh
@@ -9,13 +9,15 @@ export PYTHONUNBUFFERED=1
 export LD_LIBRARY_PATH=/data/chongyiz/anaconda3/envs/contrastive_rl/lib
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/data/chongyiz/.mujoco/mujoco200/bin
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/nvidia-000
-export CUDA_VISBLE_DEVICES=7
 export PYTHONPATH=$PROJECT_DIR
 export HDF5_USE_FILE_LOCKING=FALSE
+export XLA_FLAGS=--xla_gpu_force_compilation_parallelism=1
+export XLA_PYTHON_CLIENT_MEM_FRACTION=.50
 
 declare -a seeds=(0 1 2)
 
 for seed in "${seeds[@]}"; do
+  export CUDA_VISIBLE_DEVICES=$seed
   rm $CONDA_PREFIX/lib/python*/site-packages/mujoco_py/generated/mujocopy-buildlock
   mkdir -p ~/offline_c_learning/contrastive_rl_logs/"${EXP_LABEL}"/offline_ant_umaze/$seed
   nohup \
@@ -25,5 +27,6 @@ for seed in "${seeds[@]}"; do
     --seed="$seed" \
     --lp_launch_type=local_mt \
     --root_dir=~/offline_c_learning/contrastive_rl_logs/"${EXP_LABEL}"/offline_ant_umaze/$seed \
-  > ~/offline_c_learning/contrastive_rl_logs/"${EXP_LABEL}"/offline_ant_umaze/$seed/stream.log 2>&1 &
+  > ~/offline_c_learning/contrastive_rl_logs/"${EXP_LABEL}"/offline_ant_umaze/$seed/stream.log 2>&1 & \
+  sleep 5
 done
