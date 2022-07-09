64b15c5f840c77e21111325ff4e830c6b9458be6
Chongyi Zheng <chongyiz@Chongyis-MacBook-Pro.local>
2022-07-09 11:07:52 -0400

    2M lambda tuning

--- c_learning/bash_scripts/antmaze_medium_play_v2_offline_sarsa_mle_bc_lambda_tuning_exp.sh
+++ c_learning/bash_scripts/antmaze_medium_play_v2_offline_sarsa_mle_bc_lambda_tuning_exp.sh
@@ -22,7 +22,7 @@ for bc_lambda in "${all_bc_lambdas[@]}"; do
       --gin_bindings="train_eval_offline.agent='c_learning_agent'" \
       --gin_bindings="train_eval_offline.env_name='antmaze-medium-play-v2'" \
       --gin_bindings="train_eval_offline.random_seed=${seed}" \
-      --gin_bindings="train_eval_offline.num_iterations=1000000" \
+      --gin_bindings="train_eval_offline.num_iterations=2000000" \
       --gin_bindings="train_eval_offline.max_future_steps=350" \
       --gin_bindings="obs_to_goal.start_index=(0,)" \
       --gin_bindings="obs_to_goal.end_index=(2,)" \
