6a13ea979fb04985f2928f35091b4f57a6fd0a68
Chongyi Zheng <chongyiz@Chongyis-MacBook-Pro.local>
2022-07-14 22:43:50 -0400

    update exp scripts

--- contrastive_rl/bash_scripts/offline_ant_medium_play_exp.sh
+++ contrastive_rl/bash_scripts/offline_ant_medium_play_exp.sh
@@ -12,12 +12,12 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/nvidia-000
 export PYTHONPATH=$PROJECT_DIR
 export HDF5_USE_FILE_LOCKING=FALSE
 export XLA_FLAGS=--xla_gpu_force_compilation_parallelism=1
-export XLA_PYTHON_CLIENT_MEM_FRACTION=.30
+export XLA_PYTHON_CLIENT_MEM_FRACTION=.50
 
 declare -a seeds=(0 1 2)
 
 for seed in "${seeds[@]}"; do
-  export CUDA_VISIBLE_DEVICES=0
+  export CUDA_VISIBLE_DEVICES=3
   rm $CONDA_PREFIX/lib/python*/site-packages/mujoco_py/generated/mujocopy-buildlock
   rm -r ~/offline_c_learning/contrastive_rl_logs/offline/"${EXP_LABEL}"/offline_ant_medium_play/$seed
   mkdir -p ~/offline_c_learning/contrastive_rl_logs/offline/"${EXP_LABEL}"/offline_ant_medium_play/$seed
