9fd3260c963b0fe231729e7d370977d2e512e322
Chongyi Zheng <chongyiz@Chongyis-MacBook-Pro.local>
2022-07-17 00:35:52 -0400

    update exp scripts

--- contrastive_rl/bash_scripts/offline_ant_medium_play_sarsa_c_learning_w_clipping_tuning_exp.sh
+++ contrastive_rl/bash_scripts/offline_ant_medium_play_sarsa_c_learning_w_clipping_tuning_exp.sh
@@ -22,8 +22,8 @@ for w_clipping_idx in "${!w_clippings[@]}"; do
   for seed in "${seeds[@]}"; do
     export CUDA_VISIBLE_DEVICES=$w_clipping_idx
     rm $CONDA_PREFIX/lib/python*/site-packages/mujoco_py/generated/mujocopy-buildlock
-    rm -r ~/offline_c_learning/contrastive_rl_logs/offline/"${EXP_LABEL}"/offline_ant_medium_play/$seed
-    mkdir -p ~/offline_c_learning/contrastive_rl_logs/offline/"${EXP_LABEL}"/offline_ant_medium_play/$seed
+    rm -r ~/offline_c_learning/contrastive_rl_logs/offline/"${EXP_LABEL}"/w_clipping_"${w_clippings[$w_clipping_idx]}"/offline_ant_medium_play/$seed
+    mkdir -p ~/offline_c_learning/contrastive_rl_logs/offline/"${EXP_LABEL}"/w_clipping_"${w_clippings[$w_clipping_idx]}"/offline_ant_medium_play/$seed
     nohup \
     python $PROJECT_DIR/lp_contrastive.py \
       --env_name=offline_ant_medium_play \
@@ -31,8 +31,8 @@ for w_clipping_idx in "${!w_clippings[@]}"; do
       --seed="$seed" \
       --w_clipping="${w_clippings[$w_clipping_idx]}" \
       --lp_launch_type=local_mt \
-      --root_dir=~/offline_c_learning/contrastive_rl_logs/offline/"${EXP_LABEL}"/offline_ant_medium_play/$seed \
-    > ~/offline_c_learning/contrastive_rl_logs/offline/"${EXP_LABEL}"/offline_ant_medium_play/$seed/stream.log 2>&1 & \
+      --root_dir=~/offline_c_learning/contrastive_rl_logs/offline/"${EXP_LABEL}"/w_clipping_"${w_clippings[$w_clipping_idx]}"/offline_ant_medium_play/$seed \
+    > ~/offline_c_learning/contrastive_rl_logs/offline/"${EXP_LABEL}"/w_clipping_"${w_clippings[$w_clipping_idx]}"/offline_ant_medium_play/$seed/stream.log 2>&1 & \
     sleep 5
   done
 done
